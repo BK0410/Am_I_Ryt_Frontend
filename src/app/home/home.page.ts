6fcbe25df0cbce2ccf3d0499b10c024d05c67fbc
BARATHKUMAR M <bk@BARATHKUMARs-MacBook-Pro.local>
2022-09-24 20:55:08 +0530

    OCR

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,8 +4,8 @@ import * as Tesseract from 'tesseract.js';
 import { LoginConstants } from '../shared/constant';
 import { createWorker } from 'tesseract.js';
 //import { AnyPlugin } from 'any-plugin';
-import { Plugin} from '@capacitor/core';
-import{CameraResultType,CameraSource,Camera} from  '@capacitor/camera';
+import { Plugin } from '@capacitor/core';
+import { CameraResultType, CameraSource, Camera } from '@capacitor/camera';
 import { Plugins } from 'protractor/built/plugins';
 import { from } from 'rxjs';
 //const {Camera}=Plugins;
@@ -15,89 +15,82 @@ import { from } from 'rxjs';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-  worker:Tesseract.Worker;
-  workerReady=false;
-  image="../../assets/icon/test.png"
-  ocrResult='';
-  captureProgress=0;
-isText:boolean=true;
-isPdf:boolean=false;
-isVideo:boolean=false;
-isScan:boolean=false;
+  worker: Tesseract.Worker;
+  workerReady = false;
+  image = '../../assets/icon/test.png';
+  ocrResult = '';
+  captureProgress = 0;
+  isText: boolean = true;
+  isPdf: boolean = false;
+  isVideo: boolean = false;
+  isScan: boolean = false;
 
-  constructor(private router: Router) { 
+  constructor(private router: Router) {
     this.loadWorker();
   }
 
-  ngOnInit() {
-   
+  ngOnInit() {}
+
+  changetoTextbox() {
+    this.isText = true;
+    this.isPdf = false;
+    this.isScan = false;
+    this.isVideo = false;
   }
-  
-  changetoTextbox()
-  {
-    this.isText=true;
-    this.isPdf=false;
-    this.isScan=false;
-    this.isVideo=false;
+  changetoPdfbox() {
+    this.isText = false;
+    this.isPdf = true;
+    this.isScan = false;
+    this.isVideo = false;
   }
-  changetoPdfbox()
-  {
-    this.isText=false;
-    this.isPdf=true;
-    this.isScan=false;
-    this.isVideo=false;
+  changetoScanbox() {
+    this.isText = false;
+    this.isPdf = false;
+    this.isScan = true;
+    this.isVideo = false;
   }
-  changetoScanbox()
-  {
-    this.isText=false;
-    this.isPdf=false;
-    this.isScan=true;
-    this.isVideo=false;
-  }
-  changetoVideobox()
-  {
-    this.isText=false;
-    this.isPdf=false;
-    this.isScan=false;
-    this.isVideo=true;
+  changetoVideobox() {
+    this.isText = false;
+    this.isPdf = false;
+    this.isScan = false;
+    this.isVideo = true;
   }
   routetoQuestionspage(): void {
-    this.router.navigate(['question']).then(()=>{
+    this.router.navigate(['question']).then(() => {
       window.location.reload();
-   });
-  }
-  async loadWorker(){
-    this.worker=createWorker({
-      logger:progress =>{
-        console.log("progress")
-        if(progress.status=='recognizing text')
-        {this.captureProgress==parseInt(''+ progress.progress*100)
-      }
-      }
     });
-    console.log("inside loadworker")
+  }
+  async loadWorker() {
+    this.worker = createWorker({
+      logger: (progress) => {
+        console.log('progress');
+        if (progress.status == 'recognizing text') {
+          this.captureProgress == parseInt('' + progress.progress * 100);
+        }
+      },
+    });
+    console.log('inside loadworker');
     await this.worker.load();
-    console.log("out loadworker")
+    console.log('out loadworker');
     await this.worker.loadLanguage('eng');
     await this.worker.initialize('eng');
     console.log('Fin');
-    this.workerReady=true;
-
+    this.workerReady = true;
   }
-  async captureImage(){
-    const image=await Camera.getPhoto({
-      quality:90,
-      allowEditing:true,
-      resultType:CameraResultType.DataUrl,
-      source:CameraSource.Camera
-    })
-    console.log('image ',image)
-    this.image=image.dataUrl;
+  async captureImage() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera,
+    });
+    console.log('image ', image);
+    this.image = image.dataUrl;
   }
-  async recognizeImage(){
-    const result=await this.worker.recognize(this.image)
+  async recognizeImage() {
+    const result = await this.worker.recognize(this.image);
     console.log(result);
-    this.ocrResult=result.data.text
+    this.ocrResult = result.data.text;
   }
-  logo=LoginConstants.imgPath.logo;
+  logo = LoginConstants.imgPath.logo;
 }
